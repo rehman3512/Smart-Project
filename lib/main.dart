1fb06dcfbb851c29d3788fa4d0687ceff1bb3768
REHMAN KHAN <rehmankh2256@gmail.com>
2025-08-31 00:02:11 +0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:smartproj/View/HomeView/smartHome.dart';
+import 'package:smartproj/routes/routes.dart';
 
 void main()
 {
@@ -13,7 +13,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SmartHome(),
+      // home: ,
+      // initialRoute: "/",
+      // getPages: [
+      //   // key : value
+      //   GetPage(name: "/", page: ()=>SplashView()),
+      //   GetPage(name: "/SignInScreen", page: ()=>SignInview()),
+      //   GetPage(name: "/SmartHomeScreen", page: ()=>SmartHome()),
+      //   GetPage(name: "/SignUpScreen", page: ()=>SignUpView()),
+      // ],
+      initialRoute: AppRoutes.SplashScreen,
+      getPages: AppRoutes.routes,
+
     );
   }
 }
